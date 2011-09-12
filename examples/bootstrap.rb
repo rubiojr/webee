45ecceabaff429e8a9ba09882ffc4665779f2e96
Sergio Rubio <rubiojr@frameos.org>
2011-09-12 12:15:29 +0200

    * fixes to bootstrap.rb example

--- examples/bootstrap.rb
+++ examples/bootstrap.rb
@@ -4,6 +4,7 @@ require 'webee'
 require 'md5'
 
 CIAB_IP = ENV['host'] || '127.0.0.1'
+
 WeBee::Api.user = ENV['user'] || 'admin'
 WeBee::Api.password = ENV['pass'] || 'xabiquo'
 WeBee::Api.url = "http://#{ENV['host'] || 'abiquo'}/api"
@@ -19,16 +20,8 @@ end
 WeBee::Config.add_license File.read(license_file).strip.chomp
 
 dc = WeBee::Datacenter.create :name => 'BCN'
-ent = WeBee::Enterprise.create :name => 'QA'
-
-user = qa_ent.create_user :name => 'Sergio Rubio',
-                   :role => WeBee::UserRole.cloud_admin,
-                   :password => MD5.hexdigest('rubiojr'),
-                   :surname => 'rubiojr', #optional
-                   :email => 'rubiojr@apient.com',
-                   :locale => 'en_US',
-                   :nick => 'rubiojr',
-                   :active => 'true'  #optional
+ent = WeBee::Enterprise.all.first
+ent.set_limits_for_datacenter dc
 
 rack = dc.add_rack :name => 'rack_1',
                    :ha_enabled => false,
@@ -41,9 +34,4 @@ machine = dc.discover_machine :ip => CIAB_IP, :hypervisortype => 'kvm'
 machine.virtual_switch = machine.virtual_switches.first
 machine.datastores.first.enabled = true 
 rack.add_machine(machine)
-
-qa = WeBee::Enterprise.find_by_name('QA').first
-qa.set_limits_for_datacenter dc
-vdc = qa.create_vdc :name => 'kvm-vdc', :hypervisortype => 'KVM', :datacenter => dc
-vdc.delete
-vdc = qa.create_vdc :name => 'kvm-vdc', :hypervisortype => 'KVM', :datacenter => dc
+vdc = ent.create_vdc :name => 'kvm-vdc', :hypervisortype => 'KVM', :datacenter => dc
