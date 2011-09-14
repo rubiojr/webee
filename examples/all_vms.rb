0ff8035325bef21681986357b46c2f405563e3f8
Sergio Rubio <rubiojr@frameos.org>
2011-09-14 12:18:41 +0200

    * added all_vms.rb example

---
+++ examples/all_vms.rb
@@ -0,0 +1,27 @@
+$: << '../lib'
+require 'webee'
+require 'md5'
+require 'active_support'
+require 'term/ansicolor'
+
+class String
+  include Term::ANSIColor
+end
+
+WeBee::Api.user = ENV['user'] || 'admin'
+WeBee::Api.password = ENV['pass'] || 'xabiquo'
+WeBee::Api.url = 'http://server/api'
+
+include WeBee
+
+Datacenter.all.each do |dc|
+  dc.racks.each do |rack|
+    puts "Rack: #{rack.name}"
+    rack.machines.each do |machine|
+      puts "Machine:".green.bold + " #{machine.name} (#{machine.virtual_machines.size} VMs)"
+      machine.virtual_machines.each do |vm|
+        puts "  Name:".yellow.bold + " #{vm.name} " + "Enterprise:".yellow.bold + " #{vm.enterprise.name rescue ''} " + "VDC:".yellow.bold + " #{vm.vdc.name rescue ''}"
+      end
+    end
+  end
+end
